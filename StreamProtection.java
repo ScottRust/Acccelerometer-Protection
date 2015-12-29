ab5cc8eedeff9d545392f48aa3d4602b52caadd2
Scottocus <scottrustenhaven@googlemail.com>
2015-12-30 00:52:56 +0100

    Update StreamProtection.java

--- StreamProtection.java
+++ StreamProtection.java
@@ -1,4 +1,4 @@
-package com.scottocus.accelerometer;
+package data.stream;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,13 +16,15 @@ public class DataReaderTest
 	{
 		//Starts HID data pipe stream, reads data from the stream 
 		Runtime runtime = Runtime.getRuntime(); 
-		Process process = runtime.exec("cmd.exe /c gcdcTool --raw", null, new File("C:\\Users\\user1\\Desktop\\gcdcTool_Win7-8"));
+		Process process = runtime.exec("cmd.exe /c gcdcTool --raw", null, new File("C:\\Users\\Administrator\\Desktop\\gcdcTool_Win7-8"));
 		InputStream is = process.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
-		SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		String line;
-		int ax1 = 0, ay1 = 0, az1 = 0, timeFix, lineCount = 0;
+		int /** ax1 = 0, ay1 = 0, az1 = 0,**/ timeFix, lineCount = 0, runs = 0;
+		float ax1 = 0, ay1 = 0, az1 = 0, xBase = 0, yBase = 0, zBase = 0;
+		String ax, ay, az;
 		
 		while ((line = br.readLine()) != null)
 		{
@@ -67,57 +69,241 @@ public class DataReaderTest
 			//Converts string data to integer data for different lengths of number
 			if (line.substring(12 + timeFix, 13 + timeFix).equals(","))
 			{
-				ax1 += Integer.parseInt(line.substring(11 + timeFix, 12 + timeFix));
+				ax = line.substring(11 + timeFix, 12 + timeFix);
+				ax.replaceAll("\\s","");
+				ax1 += Integer.parseInt(ax);
 			}
 			else if (line.substring(13 + timeFix, 14 + timeFix).equals(","))
 			{
-				ax1 += Integer.parseInt(line.substring(11 + timeFix, 13 + timeFix));
+				ax = line.substring(11 + timeFix, 13 + timeFix);
+				ax.replaceAll("\\s","");
+				ax1 += Integer.parseInt(ax);
 			}
 			else if (line.substring(14 + timeFix, 15 + timeFix).equals(","))
 			{
-				ax1 += Integer.parseInt(line.substring(11 + timeFix, 14 + timeFix));
+				ax = line.substring(11 + timeFix, 14 + timeFix);
+				ax.replaceAll("\\s","");
+				ax1 += Integer.parseInt(ax);
 			}
 			else if (line.substring(15 + timeFix, 16 + timeFix).equals(","))
 			{
-				ax1 += Integer.parseInt(line.substring(11 + timeFix, 15 + timeFix));
+				ax = line.substring(11 + timeFix, 15 + timeFix);
+				ax.replaceAll("\\s","");
+				ax1 += Integer.parseInt(ax);
 			}
 			else
 			{
-				ax1 += Integer.parseInt(line.substring(11 + timeFix, 16 + timeFix));
+				ax = line.substring(11 + timeFix, 16 + timeFix);
+				ax.replaceAll("\\s","");
+				ax1 += Integer.parseInt(ax);
+			}
+			
+			if (line.substring(12 + timeFix, 13 + timeFix).equals(",") && line.substring(line.length() - 8, line.length() - 7).equals(",") && !(line.substring(line.length() - 6, line.length()).contains(",")))
+			{
+				ay = line.substring(14 + timeFix, line.length() - 8);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(13 + timeFix, 14 + timeFix).equals(",") && line.substring(line.length() - 8, line.length() - 7).equals(",") && !(line.substring(line.length() - 6, line.length()).contains(",")))
+			{
+				ay = line.substring(15 + timeFix, line.length() - 8);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(14 + timeFix, 15 + timeFix).equals(",") && line.substring(line.length() - 8, line.length() - 7).equals(",") && !(line.substring(line.length() - 6, line.length()).contains(",")))
+			{
+				ay = line.substring(16 + timeFix, line.length() - 8);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(15 + timeFix, 16 + timeFix).equals(",") && line.substring(line.length() - 8, line.length() - 7).equals(",") && !(line.substring(line.length() - 6, line.length()).contains(",")))
+			{
+				ay = line.substring(17 + timeFix, line.length() - 8);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(16 + timeFix, 17 + timeFix).equals(",") && line.substring(line.length() - 8, line.length() - 7).equals(",") && !(line.substring(line.length() - 6, line.length()).contains(",")))
+			{
+				ay = line.substring(18 + timeFix, line.length() - 8);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(12 + timeFix, 13 + timeFix).equals(",") && line.substring(line.length() - 7, line.length() - 6).equals(",") && !(line.substring(line.length() - 5, line.length()).contains(",")))
+			{
+				ay = line.substring(14 + timeFix, line.length() - 7);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(13 + timeFix, 14 + timeFix).equals(",") && line.substring(line.length() - 7, line.length() - 6).equals(",") && !(line.substring(line.length() - 5, line.length()).contains(",")))
+			{
+				ay = line.substring(15 + timeFix, line.length() - 7);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(14 + timeFix, 15 + timeFix).equals(",") && line.substring(line.length() - 7, line.length() - 6).equals(",") && !(line.substring(line.length() - 5, line.length()).contains(",")))
+			{
+				ay = line.substring(16 + timeFix, line.length() - 7);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(15 + timeFix, 16 + timeFix).equals(",") && line.substring(line.length() - 7, line.length() - 6).equals(",") && !(line.substring(line.length() - 5, line.length()).contains(",")))
+			{
+				ay = line.substring(17 + timeFix, line.length() - 7);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(16 + timeFix, 17 + timeFix).equals(",") && line.substring(line.length() - 7, line.length() - 6).equals(",") && !(line.substring(line.length() - 5, line.length()).contains(",")))
+			{
+				ay = line.substring(18 + timeFix, line.length() - 7);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(12 + timeFix, 13 + timeFix).equals(",") && line.substring(line.length() - 6, line.length() - 5).equals(",") && !(line.substring(line.length() - 4, line.length()).contains(",")))
+			{
+				ay = line.substring(14 + timeFix, line.length() - 6);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(13 + timeFix, 14 + timeFix).equals(",") && line.substring(line.length() - 6, line.length() - 5).equals(",") && !(line.substring(line.length() - 4, line.length()).contains(",")))
+			{
+				ay = line.substring(15 + timeFix, line.length() - 6);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(14 + timeFix, 15 + timeFix).equals(",") && line.substring(line.length() - 6, line.length() - 5).equals(",") && !(line.substring(line.length() - 4, line.length()).contains(",")))
+			{
+				ay = line.substring(16 + timeFix, line.length() - 6);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(15 + timeFix, 16 + timeFix).equals(",") && line.substring(line.length() - 6, line.length() - 5).equals(",") && !(line.substring(line.length() - 4, line.length()).contains(",")))
+			{
+				ay = line.substring(17 + timeFix, line.length() - 6);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(16 + timeFix, 17 + timeFix).equals(",") && line.substring(line.length() - 6, line.length() - 5).equals(",") && !(line.substring(line.length() - 4, line.length()).contains(",")))
+			{
+				ay = line.substring(18 + timeFix, line.length() - 6);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(12 + timeFix, 13 + timeFix).equals(",") && line.substring(line.length() - 5, line.length() - 4).equals(",") && !(line.substring(line.length() - 3, line.length()).contains(",")))
+			{
+				ay = line.substring(14 + timeFix, line.length() - 5);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(13 + timeFix, 14 + timeFix).equals(",") && line.substring(line.length() - 5, line.length() - 4).equals(",") && !(line.substring(line.length() - 3, line.length()).contains(",")))
+			{
+				ay = line.substring(15 + timeFix, line.length() - 5);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(14 + timeFix, 15 + timeFix).equals(",") && line.substring(line.length() - 5, line.length() - 4).equals(",") && !(line.substring(line.length() - 3, line.length()).contains(",")))
+			{
+				ay = line.substring(16 + timeFix, line.length() - 5);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(15 + timeFix, 16 + timeFix).equals(",") && line.substring(line.length() - 5, line.length() - 4).equals(",") && !(line.substring(line.length() - 3, line.length()).contains(",")))
+			{
+				ay = line.substring(17 + timeFix, line.length() - 5);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(16 + timeFix, 17 + timeFix).equals(",") && line.substring(line.length() - 5, line.length() - 4).equals(",") && !(line.substring(line.length() - 3, line.length()).contains(",")))
+			{
+				ay = line.substring(18 + timeFix, line.length() - 5);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(12 + timeFix, 13 + timeFix).equals(",") && line.substring(line.length() - 4, line.length() - 3).equals(","))
+			{
+				ay = line.substring(14 + timeFix, line.length() - 4);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(13 + timeFix, 14 + timeFix).equals(",") && line.substring(line.length() - 4, line.length() - 3).equals(","))
+			{
+				ay = line.substring(15 + timeFix, line.length() - 4);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(14 + timeFix, 15 + timeFix).equals(",") && line.substring(line.length() - 4, line.length() - 3).equals(","))
+			{
+				ay = line.substring(16 + timeFix, line.length() - 4);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(15 + timeFix, 16 + timeFix).equals(",") && line.substring(line.length() - 4, line.length() - 3).equals(","))
+			{
+				ay = line.substring(17 + timeFix, line.length() - 4);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
+			}
+			else if (line.substring(16 + timeFix, 17 + timeFix).equals(",") && line.substring(line.length() - 4, line.length() - 3).equals(","))
+			{
+				ay = line.substring(18 + timeFix, line.length() - 4);
+				ay.replaceAll("\\s","");
+				ay1 += Integer.parseInt(ay);
 			}
 			
 			if (line.substring(line.length() - 8, line.length() - 7).equals(",") && !(line.substring(line.length() - 6, line.length()).contains(",")))
 			{
-				az1 += Integer.parseInt(line.substring(line.length() - 6, line.length()));
+				az = line.substring(line.length() - 6, line.length());
+				az.replaceAll("\\s","");
+				az1 += Integer.parseInt(az);
 			}
 			else if (line.substring(line.length() - 7, line.length() - 6).equals(",") && !(line.substring(line.length() - 5, line.length()).contains(",")))
 			{
-				az1 += Integer.parseInt(line.substring(line.length() - 5, line.length()));
+				az = line.substring(line.length() - 5, line.length());
+				az.replaceAll("\\s","");
+				az1 += Integer.parseInt(az);
 			}
 			else if (line.substring(line.length() - 6, line.length() - 5).equals(",") && !(line.substring(line.length() - 4, line.length()).contains(",")))
 			{
-				az1 += Integer.parseInt(line.substring(line.length() - 4, line.length()));
+				az = line.substring(line.length() - 4, line.length());
+				az.replaceAll("\\s","");
+				az1 += Integer.parseInt(az);
 			}
 			else if (line.substring(line.length() - 5, line.length() - 4).equals(",") && !(line.substring(line.length() - 3, line.length()).contains(",")))
 			{
-				az1 += Integer.parseInt(line.substring(line.length() - 3, line.length()));
+				az = line.substring(line.length() - 3, line.length());
+				az.replaceAll("\\s","");
+				az1 += Integer.parseInt(az);
 			}
 			else if (line.substring(line.length() - 4, line.length() - 3).equals(","))
 			{
-				az1 += Integer.parseInt(line.substring(line.length() - 2, line.length()));
+				az = line.substring(line.length() - 2, line.length());
+				az.replaceAll("\\s","");
+				az1 += Integer.parseInt(az);
 			}
 			else if (line.substring(line.length() - 3, line.length() - 2).equals(","))
 			{
-				az1 += Integer.parseInt(line.substring(line.length() - 1, line.length()));
+				az = line.substring(line.length() - 1, line.length());
+				az.replaceAll("\\s","");
+				az1 += Integer.parseInt(az);
 			}
 			else
 			{
-				az1 += Integer.parseInt(line.substring(line.length() - 7, line.length()));
+				az = line.substring(line.length() - 7, line.length());
+				az.replaceAll("\\s","");
+				az1 += Integer.parseInt(az);
 			}
 			
-		    /**if(lineCount == 500)
-		    {
-		    	if(Math.abs(ax1 - -500) < 200 || Math.abs(ax1 - -500) > 500)
+			if(lineCount == 250)
+			{
+				if (runs == 0)
+				{
+					xBase = Math.abs(ax1/250);
+					yBase = Math.abs(ay1/250);
+					zBase = Math.abs(az1/250);
+				}
+				System.out.println(Math.abs(ax1/250) - xBase);
+				System.out.println(Math.abs(ay1/250) - yBase);
+				System.out.println(Math.abs(az1/250) - zBase);
+				if(((Math.abs(Math.abs(ax1/250) - xBase)) > 1.2) || ((Math.abs(Math.abs(ay1/250) - yBase)) > 1.2) || (((Math.abs(Math.abs(az1/250))) - zBase) > 1.2))
 		    	{
 		        	java.awt.Toolkit.getDefaultToolkit().beep();
 			    	System.out.println("ALERT!");
@@ -129,18 +315,12 @@ public class DataReaderTest
 			        out.println();
 			        out.close();
 			    	//runtime.exec("cmd.exe /c shutdown -s");
-			    	ax1 = 0;
-			    	az1 = 0;
 		    	}
-		    }**/
-			
-			if(lineCount == 500)
-			{
-				System.out.println(Math.abs(ax1/** - -500**/));
-				System.out.println(Math.abs(az1/** - -1016000**/));		        
 				ax1 = 0;
+				ay1 = 0;
 				az1 = 0;
 				lineCount = -1;
+				runs++;
 			}
 			lineCount++;
 		}
